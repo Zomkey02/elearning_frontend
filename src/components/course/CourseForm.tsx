636b54c0dd73faed9eb13e1276a7bd1cfba72e74
Zomkey02 <zomkey.wien@hotmail.com>
2025-08-16 03:00:49 +0200

    improve form layout of course and lessons

--- src/components/course/CourseForm.tsx
+++ src/components/course/CourseForm.tsx
@@ -51,49 +51,59 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit, defaultValues, mode }
   return (
     <form 
       onSubmit={handleSubmit(onSubmit)}
-      className='w-full max-w-xl p-4 mx-auto space-y-6'>
+      className='w-full max-w-3xl p-4 mx-auto space-y-6'>
         <h2 className='text-2xl font-bold text-center'>
           {mode === 'create' ? 'Create New Course' : 'Update Course'}
         </h2>
 
         <div>
-          <input 
-            {...register('title', { required: 'Title is required' })} 
-            placeholder='Title' 
-            className='w-full h-14 rounded-xl p-4 bg-gray-200 placeholder-[#5c8a5c] text-base font-normal focus:ring-0' 
+          <input {...register('title', { required: 'Title is required' })} 
+            placeholder='Title' className='form-input' 
           />
-          {errors.title && <p className='text-red-500'>{errors.title.message}</p>}
+          {errors.title && <p className='form-error'>{errors.title.message}</p>}
         </div>
 
-        <div>
-          <input 
-            {...register('slug')} 
-            placeholder='Slug' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
+        <div className='hidden'>
+          <input {...register('slug')} 
+            placeholder='Slug' className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
           />
-        </div>
+        </div> 
 
-        <div>
-          <select 
-            {...register('category', { required: 'Category is required' })} 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl'
-          >
-              <option value='investing-basics'>Investing Basics</option>
-              <option value='passive-investing-strategies'>Passive Investing Strategies</option>
-              <option value='personal-finance'>Personal Finance</option>
-          </select>
-          {errors.category && <p className='text-red-500'>{errors.category.message}</p>}
-        </div>
+        <div className='inline-flex w-full '>
+          <div className='w-1/2 mr-3'>
+            <select 
+              {...register('category', { required: 'Category is required' })} 
+              className='form-select'
+            >
+                <option value='investing-basics'>Investing Basics</option>
+                <option value='passive-investing-strategies'>Passive Investing Strategies</option>
+                <option value='personal-finance'>Personal Finance</option>
+            </select>
+            {errors.category && <p className='form-error'>{errors.category.message}</p>}
+          </div>
 
-        <div>
-          <input 
-            {...register('summary', { required: 'Summary is required' })} 
-            placeholder='Summary' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
-          />
-          {errors.summary && <p className='text-red-500'>{errors.summary.message}</p>}
-        </div>
+          <div className='w-1/4 mr-3'>
+            <input 
+              type='number'
+              {...register('duration', { required: 'Duration is required', valueAsNumber:true })} 
+              placeholder='Duration' 
+              className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
+            />
+            {errors.duration && <p className='form-error'>{errors.duration.message}</p>}
+          </div>
 
+          <div className='w-1/4'>
+            <select 
+              {...register('status', { required: 'Status is required' })} 
+              className='w-full p-4 bg-gray-200 h-14 rounded-xl'
+            >
+                <option value='' disabled>Select Status</option>
+                <option value='draft'>Draft</option>
+                <option value='published'>Publish</option>
+            </select>
+            {errors.status && <p className='form-error'>{errors.status.message}</p>}
+          </div>
+        </div>
         
         {defaultValues?.thumbnailUrl && (
           <div className="mb-4">
@@ -107,25 +117,25 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit, defaultValues, mode }
         )}
 
         <div>
-          <label className='flex flex-col items-center rounded-xl border-2 border-dashed border-[#d4e2d4] px-6 py-14 cursor-pointer bg-[#6DAE81] text-center'>
-            <p className='mb-1 text-lg font-bold tracking-tight'>Upload Thumbnail</p>
-            <p className='max-w-xs text-sm'>Click to upload</p>
+          <label className='max-h-[100px] flex flex-col items-center justify-center px-6 text-center cursor-pointer rounded-xl py-14 bg-primary'>
+            <p className='mb-1 text-lg font-bold tracking-tight text-white'>Upload Thumbnail</p>
+            <p className='text-sm text-white '>Click to upload</p>
             <input type="file" className='hidden'
               {...register('thumbnail', {required: mode === 'create' ? 'Thumbnail is required' : false})} 
             />
           </label>
-          
-          {errors.thumbnail && <p className='mt-1 text-sm text-red-500'>{errors.thumbnail.message}</p>}
+          {errors.thumbnail && <p className='form-error'>{errors.thumbnail.message}</p>}
         </div>
 
-{/*         <div>
+        <div>
           <textarea 
-            {...register('description', { required: 'Description is required' })} 
-            placeholder='Description' 
-            className='w-full min-h-[144px] p-4 bg-gray-200 rounded-xl text-base font-normal resize-none focus:outline-none focus:ring-0'>  
-          </textarea>
-          {errors.description && <p className='text-red-500'>{errors.description.message}</p>}
-        </div> */}
+            {...register('summary', { required: 'Summary is required' })} 
+            placeholder='Summary' 
+            className='form-input min-h-[100px]' 
+          />
+          {errors.summary && <p className='form-error'>{errors.summary.message}</p>}
+        </div>
+        
         <div>
           <Controller 
             name='description'
@@ -138,38 +148,16 @@ const CourseForm: React.FC<CourseFormProps> = ({ onSubmit, defaultValues, mode }
                   onChange={field.onChange} 
                   placeholder='Write your description' 
                 />
-                {errors.description && <p className='text-red-500'>{errors.description.message}</p>}
+                {errors.description && <p className='form-error'>{errors.description.message}</p>}
               </>
             )}
           />
         </div>
 
-        <div>
-          <input 
-            type='number'
-            {...register('duration', { required: 'Duration is required', valueAsNumber:true })} 
-            placeholder='Duration' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
-          />
-          {errors.duration && <p className='text-red-500'>{errors.duration.message}</p>}
-        </div>
-
-        <div>
-          <select 
-            {...register('status', { required: 'Status is required' })} 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl'
-          >
-              <option value='' disabled>Select Status</option>
-              <option value='draft'>Draft</option>
-              <option value='published'>Publish</option>
-          </select>
-          {errors.status && <p className='text-red-500'>{errors.status.message}</p>}
-        </div>
-
         <button 
           disabled={isSubmitting} 
           type='submit' 
-          className='w-full h-14 bg-[#6DAE81] text-white rounded-xl hover:bg-[#5aa170] transition'>
+          className='w-full h-12 btn-secondary'>
           {mode === 'create' ? 'Create Course' : 'Update Course'}
         </button>
     </form>
--- src/components/lesson/LessonForm.tsx
+++ src/components/lesson/LessonForm.tsx
@@ -45,7 +45,7 @@ const LessonForm: React.FC<LessonFormProps> = ({ onSubmit, defaultValues, mode, 
 
 
 return (
-    <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-xl mx-auto space-y-4'>
+    <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-3xl mx-auto space-y-4'>
         <h2 className='text-2xl font-bold text-center'>
             {mode === 'create' ? 'Create a new lesson' : 'Update a lesson'}
         </h2>
@@ -57,6 +57,7 @@ return (
             <select 
               {...register('course_id', {required:'Course is required'})}
               defaultValue={defaultValues?.course_id}
+              className='form-select'
             >
                 {courses.map((course) => (
                     <option key={course.id} value={course.id}>
@@ -65,24 +66,65 @@ return (
                 ))}
             </select>
           )}
-            {errors.course_id && <p className='text-red-500'>{errors.course_id.message}</p>}
+            {errors.course_id && <p className='form-error'>{errors.course_id.message}</p>}
         </div>
 
         <div>
-          <input 
-            {...register('title', { required: 'Title is required' })} 
-            placeholder='Title' 
-            className='w-full h-14 rounded-xl p-4 bg-gray-200 placeholder-[#5c8a5c] text-base font-normal focus:ring-0' 
-          />
-          {errors.title && <p className='text-red-500'>{errors.title.message}</p>}
+          <input {...register('title', { required: 'Title is required' })} placeholder='Title' className='form-input' />
+          {errors.title && <p className='form-error'>{errors.title.message}</p>}
+        </div>
+
+        <div className='inline-flex justify-between w-full'>
+          <div>
+            <select 
+              {...register('layout_type', { required: 'Layout type is required' })} 
+              className='pl-5 pr-5 form-select'
+            >
+                <option value='' disabled>Select Layout Type</option>
+                <option value='standard'>Standard</option>
+                <option value='video-focused'>Video Focused</option>
+                <option value='image-left'>Image Left</option>
+                <option value='interactive'>Interactive</option>
+            </select>
+            {errors.layout_type && <p className='form-error'>{errors.layout_type.message}</p>}
+          </div>
+
+          <div className='flex flex-col'>
+            <input 
+              type='number'
+              {...register('duration', { required: 'Duration is required', valueAsNumber:true })} 
+              placeholder='Duration' className='form-input max-w-[150px]' 
+            />
+            {errors.duration && <p className='form-error'>{errors.duration.message}</p>}
+          </div>
+
+          <div>
+            <select 
+              {...register('level', { required: 'Level is required' })} 
+              className='pl-5 pr-5 form-select'
+            >
+                <option value='beginner'>Beginner</option>
+                <option value='intermediate'>Intermediate</option>
+                <option value='advanced'>Advanced</option>
+            </select>
+            {errors.level && <p className='form-error'>{errors.level.message}</p>}
+          </div>
+
+          <div>
+            <select 
+              {...register('status', { required: 'Status is required' })} 
+              className='pl-5 pr-5 form-select'
+            >
+                <option value='' disabled>Select Status</option>
+                <option value='draft'>Draft</option>
+                <option value='published'>Publish</option>
+            </select>
+            {errors.status && <p className='form-error'>{errors.status.message}</p>}
+          </div>
         </div>
 
         <div>
-          <input 
-            {...register('slug')} 
-            placeholder='Slug' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
-          />
+          <input type='hidden' {...register('slug')} placeholder='Slug' className='form-input' />
         </div>
 
         {defaultValues?.thumbnailUrl && (
@@ -97,34 +139,23 @@ return (
         )}
 
         <div>
-          <label className='flex flex-col items-center rounded-xl px-6 py-14 cursor-pointer bg-[#6DAE81] text-center'>
+          <label className='max-h-[100px] flex flex-col items-center justify-center px-6 text-center cursor-pointer rounded-xl py-14 bg-primary'>
             <p className='mb-1 text-lg font-bold tracking-tight text-white'>Upload Thumbnail</p>
-            <p className='max-w-xs text-sm text-white'>Click to upload</p>
+            <p className='text-sm text-white '>Click to upload</p>
             <input type="file" className='hidden'
               {...register('thumbnail', {required: mode === 'create' ? 'Thumbnail is required' : false})} 
             />
           </label>
-          {errors.thumbnail && <p className='mt-1 text-sm text-red-500'>{errors.thumbnail.message}</p>}
+          {errors.thumbnail && <p className='form-error'>{errors.thumbnail.message}</p>}
         </div>
 
         <div>
-          <input 
+          <textarea 
             {...register('summary', { required: 'Summary is required' })} 
-            placeholder='Summary' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
+            placeholder='Summary' className='form-input min-h-[100px]' 
           />
-          {errors.summary && <p className='text-red-500'>{errors.summary.message}</p>}
+          {errors.summary && <p className='form-error'>{errors.summary.message}</p>}
         </div>
-
-
-{/*         <div>
-            <textarea
-              {...register('content', { required: 'Content is required' })}
-              placeholder='Content'
-              className='w-full min-h-[144px] p-4 bg-gray-200 rounded-xl text-base font-normal resize-none focus:outline-none focus:ring-0'
-            />
-            {errors.content && <p className='text-red-500'>{errors.content.message}</p>}
-        </div> */}
 
         <div>
           <Controller 
@@ -138,64 +169,16 @@ return (
                   onChange={field.onChange} 
                   placeholder='Write your content' 
                 />
-                {errors.content && <p className='text-red-500'>{errors.content.message}</p>}
+                {errors.content && <p className='form-error'>{errors.content.message}</p>}
               </>
             )}
           />
         </div>
 
-        <div>
-          <input 
-            type='number'
-            {...register('duration', { required: 'Duration is required', valueAsNumber:true })} 
-            placeholder='Duration' 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl' 
-          />
-          {errors.duration && <p className='text-red-500'>{errors.duration.message}</p>}
-        </div>
-
-        <div>
-          <select 
-            {...register('level', { required: 'Level is required' })} 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl'
-          >
-              <option value='beginner'>Beginner</option>
-              <option value='intermediate'>Intermediate</option>
-              <option value='advanced'>Advanced</option>
-          </select>
-          {errors.level && <p className='text-red-500'>{errors.level.message}</p>}
-        </div>
-
-        <div>
-          <select 
-            {...register('status', { required: 'Status is required' })} 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl'
-          >
-              <option value='' disabled>Select Status</option>
-              <option value='draft'>Draft</option>
-              <option value='published'>Publish</option>
-          </select>
-          {errors.status && <p className='text-red-500'>{errors.status.message}</p>}
-        </div>
-
-        <div>
-          <select 
-            {...register('layout_type', { required: 'Layout type is required' })} 
-            className='w-full p-4 bg-gray-200 h-14 rounded-xl'
-          >
-              <option value='' disabled>Select Layout Type</option>
-              <option value='standard'>Standard</option>
-              <option value='video-focused'>Video Focused</option>
-              <option value='image-left'>Image Left</option>
-              <option value='interactive'>Interactive</option>
-          </select>
-          {errors.layout_type && <p className='text-red-500'>{errors.layout_type.message}</p>}
-        </div>
-
         <button
             disabled={isSubmitting}
             type='submit'
-            className='w-full h-14 bg-[#6DAE81] text-white rounded-xl hover:bg-[#5aa170] transition'
+            className='w-full h-12 btn-secondary'
             >
                 {mode === 'create' ? 'Create Lesson' : 'Update Lesson'}
         </button>
